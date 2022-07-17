489499b582ddeb2b0d18426f9f22c40229f46c80
glULTRA <7amakurd2002@gmail.com>
2022-07-17 16:42:32 +0300

    New configurations for SandBox and Ultra solution

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,5 @@
 workspace "Ultra"
-	architecture "x64"
+	architecture  "x64"
 
 	configurations{
 		"Debug",
@@ -9,51 +9,83 @@ workspace "Ultra"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+
 project "Ultra"
 	location "Ultra"
-	language "C++"
 	kind "SharedLib"
-	targetdir ("bin/" .. outputdir .. "%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "%{prj.name}")
+	language "C++"
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/" .. outputdir .. "/%{prj.name}")
+	
+	files{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs{
+		"%{prj.name}/vendor/spdlog/include"
+	}
 
 	filter "system:windows"
 		cppdialect "C++20"
 		staticruntime "On"
 		systemversion "10.0"
+
+		defines{
+			"ULT_PLATFORM_WINDOWS",
+			"ULT_BUILD_DLL"
+		}
 
 		postbuildcommands{
 			("{copy} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/SandBox")
 		}
-
 	filter "configurations:Debug"
-		defines "HZ_DEBUG"
+		defines "ULT_DEBUG"
 		optimize  "On"
 	filter "configurations:Release"
-		defines "HZ_RELEASE"
+		defines "ULT_RELEASE"
 		optimize  "On"
 	filter "configurations:Dist"
-		defines "HZ_DIST"
+		defines "ULT_DIST"
 		optimize  "On"
 
 project "SandBox"
 	location "SandBox"
-	language "C++"
 	kind "ConsoleApp"
+	language "C++"
 
-	targetdir ("bin/" .. outputdir .. "%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/" .. outputdir .. "/%{prj.name}")
+
+	files{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs{
+		"Ultra/vendor/spdlog/include",
+		"Ultra/src"
+	}
 
 	filter "system:windows"
 		cppdialect "C++20"
 		staticruntime "On"
 		systemversion "10.0"
 
+		defines{
+			"ULT_PLATFORM_WINDOWS"
+		}
+
+	links{
+		"Ultra"
+	}
+
 	filter "configurations:Debug"
-		defines "HZ_DEBUG"
+		defines "ULT_DEBUG"
 		optimize  "On"
 	filter "configurations:Release"
-		defines "HZ_RELEASE"
+		defines "ULT_RELEASE"
 		optimize  "On"
 	filter "configurations:Dist"
-		defines "HZ_DIST"
+		defines "ULT_DIST"
 		optimize  "On"
