bf7fd920b7233f5a4bab62013ae1c2a9dfa51f57
glULTRA <7amakurd2002@gmail.com>
2022-07-20 15:16:23 +0300

    Added platform windows!

--- Ultra/src/Ultra/Core.h
+++ Ultra/src/Ultra/Core.h
@@ -7,7 +7,14 @@
 		#define ULT_API __declspec(dllimport)
 	#endif
 #else
-	
+	#error ULT Supports windows for now!
+#endif
+
+#ifdef ULT_ENABLE_ASSERTS
+	#define ULT_ASSERT(x, ...) {}
+#else
+	#define ULT_ASSERT(x, ...)
+	#define ULT_CORE_ASSERT(x, ...)
 #endif
 
 #define BIT(x) (1 << x)
