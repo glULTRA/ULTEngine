bad117e03a86eb4533cfa8b7b1b5fabaa503f918
glULTRA <7amakurd2002@gmail.com>
2022-07-20 15:58:20 +0300

    Created an object of the window platform

--- Ultra/src/Ultra/Application.cpp
+++ Ultra/src/Ultra/Application.cpp
@@ -2,6 +2,7 @@
 #include "Application.h"
 
 #include "Events/ApplicationEvent.h"
+#include "GLFW/glfw3.h"
 
 namespace Ultra {
 	Application::Application()
@@ -11,10 +12,9 @@ namespace Ultra {
 
 	void Application::Run()
 	{
-		/*WindowResizeEvent e(1280, 768);
-		if(e.IsInCategory(EventCategoryApplication))
-			ULT_TRACE(e.ToString());*/
 		while (m_Running) {
+			glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
 	}
