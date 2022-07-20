fd05b837914e7a7731b9b7a037b18ef54712cbb9
glULTRA <7amakurd2002@gmail.com>
2022-07-20 23:28:36 +0300

    New example of layers

--- SandBox/src/Source.cpp
+++ SandBox/src/Source.cpp
@@ -1,9 +1,29 @@
 #include <Ultra.h>
 
+class ExampleLayer : public Ultra::Layer
+{
+public:
+	ExampleLayer()
+		: Layer("Example")
+	{
+
+	}
+
+	void OnUpdate() override {
+		//ULT_INFO("My update");
+	}
+
+	void OnEvent(Ultra::Event& e) override
+	{
+		//std::cout << e.ToString() << std::endl;
+		ULT_WARN("{0}", e.ToString());
+	}
+};
+
 class SandBox : public Ultra::Application {
 public:
 	SandBox() {
-
+		PushLayer(new ExampleLayer());
 	}
 
 	~SandBox() {
