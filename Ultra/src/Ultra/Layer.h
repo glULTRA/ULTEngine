3a0d3ac7899b420c35b5c859d836a8389a489d04
glULTRA <7amakurd2002@gmail.com>
2022-07-20 22:18:15 +0300

    Added layers

---
+++ Ultra/src/Ultra/Layer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Events/Event.h"
+
+namespace Ultra 
+{
+	class ULT_API Layer
+	{
+	public:
+		Layer(const std::string name = "Layer");
+		virtual ~Layer();
+
+		virtual void OnAttach() {}
+		virtual void OnDetach() {}
+		virtual void OnUpdate() {}
+		virtual void OnEvent(Event& e) {}
+
+		inline const std::string& GetName() const { return m_DebugName; }
+
+	protected:
+		std::string m_DebugName;
+	};
+}
+
